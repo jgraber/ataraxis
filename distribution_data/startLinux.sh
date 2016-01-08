d0e42bd97c003efd0edbb45f705a303a6ad962bf
Johnny Graber <jg@jgraber.ch>
2016-01-08 19:05:33 +0100

    update BouncyCastle to 1.54

--- distribution_data/startLinux.sh
+++ distribution_data/startLinux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -classpath lib/swt_linux.jar:AtaraxiS.jar:lib/log4j-1.2-api-2.4.1.jar:lib/log4j-api-2.4.1.jar:lib/log4j-core-2.4.1.jar:lib/bcprov-jdk15on-153.jar:lib/jaxen-1.1.4.jar:lib/jdom-2.0.6.jar -Dlog4j.configurationFile=application_data/config/log4j2.xml ataraxis.gui.AtaraxisStarter
+java -classpath lib/swt_linux.jar:AtaraxiS.jar:lib/log4j-1.2-api-2.4.1.jar:lib/log4j-api-2.4.1.jar:lib/log4j-core-2.4.1.jar:lib/bcprov-jdk15on-154.jar:lib/jaxen-1.1.4.jar:lib/jdom-2.0.6.jar -Dlog4j.configurationFile=application_data/config/log4j2.xml ataraxis.gui.AtaraxisStarter
