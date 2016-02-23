a49ecaf4660215da0d0eab20fd4e1f49151c69c1
Johnny Graber <jg@jgraber.ch>
2016-02-23 20:25:52 +0100

    set next release date

--- src/ataraxis/gui/content/AtaraxisInfoGUI.java
+++ src/ataraxis/gui/content/AtaraxisInfoGUI.java
@@ -66,7 +66,7 @@ public class AtaraxisInfoGUI
 	// Information strings
 	private static final String VERSION = "1.7.0";
 	private static final String AUTHOR = "Johnny Graber + Andreas Muedespacher";
-	private static final String DATE = "3.2.2016";
+	private static final String DATE = "27.2.2016";
 	private static final String WEBSITE = "http://github.com/jgraber/ataraxis/";
 	
 	// mapping of languages
