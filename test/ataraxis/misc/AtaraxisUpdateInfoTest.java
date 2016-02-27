8a1b3af15aacfb2925f3121a12d4906b6614a150
Johnny Graber <jg@jgraber.ch>
2016-02-27 11:58:33 +0100

    now check for 1.7.0 as the current version

--- test/ataraxis/misc/AtaraxisUpdateInfoTest.java
+++ test/ataraxis/misc/AtaraxisUpdateInfoTest.java
@@ -49,7 +49,7 @@ public class AtaraxisUpdateInfoTest
 	private static final Logger logger = LogManager.getLogger(AtaraxisUpdateInfoTest.class);
 	private static final String networkConfigFile =  System.getProperty("user.dir") + 
 	"/application_data/config/network.properties";
-	private static final String currentVersion = "1.6.0";
+	private static final String currentVersion = "1.7.0";
 	private static final String oldVersion = "1.1.0";
 	private static final String nextMajorVersion = "2.0.0";
 	
