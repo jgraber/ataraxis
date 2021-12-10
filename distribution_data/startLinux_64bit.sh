ce10d088336acfb64255b16d0e1d35e34c8edbb6
Johnny Graber <jg@jgraber.ch>
2021-12-10 21:39:07 +0100

    update Log4j to 2.15.0

--- distribution_data/startLinux_64bit.sh
+++ distribution_data/startLinux_64bit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -classpath lib/swt_linux_64.jar:AtaraxiS.jar:lib/log4j-1.2-api-2.13.3.jar:lib/log4j-api-2.13.3.jar:lib/log4j-core-2.13.3.jar:lib/bcprov-jdk15on-167.jar:lib/jaxen-1.2.0.jar:lib/jdom-2.0.6.jar -Dlog4j.configurationFile=application_data/config/log4j2.xml ataraxis.gui.AtaraxisStarter
+java -classpath lib/swt_linux_64.jar:AtaraxiS.jar:lib/log4j-1.2-api-2.15.0.jar:lib/log4j-api-2.15.0.jar:lib/log4j-core-2.15.0.jar:lib/bcprov-jdk15on-167.jar:lib/jaxen-1.2.0.jar:lib/jdom-2.0.6.jar -Dlog4j.configurationFile=application_data/config/log4j2.xml ataraxis.gui.AtaraxisStarter
--- distribution_data/startLinux_LocalJava_64bit.sh
+++ distribution_data/startLinux_LocalJava_64bit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./jre-linux/bin/java -classpath lib/swt_linux_64.jar:AtaraxiS.jar:lib/log4j-1.2-api-2.13.3.jar:lib/log4j-api-2.13.3.jar:lib/log4j-core-2.13.3.jar:lib/bcprov-jdk15on-167.jar:lib/jaxen-1.2.0.jar:lib/jdom-2.0.6.jar -Dlog4j.configurationFile=application_data/config/log4j2.xml ataraxis.gui.AtaraxisStarter
+./jre-linux/bin/java -classpath lib/swt_linux_64.jar:AtaraxiS.jar:lib/log4j-1.2-api-2.15.0.jar:lib/log4j-api-2.15.0.jar:lib/log4j-core-2.15.0.jar:lib/bcprov-jdk15on-167.jar:lib/jaxen-1.2.0.jar:lib/jdom-2.0.6.jar -Dlog4j.configurationFile=application_data/config/log4j2.xml ataraxis.gui.AtaraxisStarter
